69615b8650bea44ba8048473651d121fc8d4f20f
SamLiu <liuyibi.work@gmail.com>
2026-03-20 21:19:31 +0800

    refactor: 移除全屏按钮，添加双击全屏功能
    - 移除左下角的全屏图标按钮
    - 双击视频区域可进入全屏

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -249,6 +249,7 @@ export default function VideoPlayer({ currentCameraIndex, onTimeUpdate, selected
                     onError={handleError}
                     onLoadedData={handleLoadedData}
                     onEnded={handleEnded}
+                    onDoubleClick={toggleFullscreen}
                 >
                     您的浏览器不支持 video 标签
                 </video>
@@ -278,14 +279,6 @@ export default function VideoPlayer({ currentCameraIndex, onTimeUpdate, selected
                     </div>
                 )}
 
-                {/* Video Controls */}
-                <div className="video-controls">
-                    <button className="control-btn" onClick={toggleFullscreen} title="全屏">
-                        <svg className="icon-fullscreen" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
-                        </svg>
-                    </button>
-                </div>
             </div>
         </div>
     );
